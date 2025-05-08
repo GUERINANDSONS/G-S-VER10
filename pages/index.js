b71627179326b5555c14f7689f1e0eb938c51225
GUERINANDSONS <chris@guerinandsons.com>
2025-05-07 21:29:58 -0700

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,1 @@
-import dynamic from 'next/dynamic';
-const Scheduler = dynamic(() => import('../components/SchedulerApp'), { ssr: false });
-
-export default function Home() {
-  return (
-    <main>
-      <h1>Custom Home Scheduler</h1>
-      <Scheduler />
-    </main>
-  );
-}
+// placeholder to be overwritten after reset
